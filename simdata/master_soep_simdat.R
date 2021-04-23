c8be830194e4fb28d723e4b3a65278d8547bcdda
Rike <f-becker@posteo.de>
2021-04-23 11:06:53 +0200

    add R code for simulated data

---
+++ simdata/master_soep_simdat.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+library(gamlss)
+
+source("C:/Users/rike/Desktop/OwnCloud Sync/_R/soep_simdata_fct.R")
+sdatm <- read.csv("C:/Users/rike/Desktop/OwnCloud Sync/_SOEP_Data/soepm.csv")
+
+#age group variable
+cutage <- c(0, seq(20, 100, by = 10))
+sdatm$agegroup <- cut(sdatm$age, breaks = cutage, label = 1:9)
+
+
+####categorical var's distributions####
+#now, find the 'true' distribution of the categorical variables 
+#conditioned on age from the data
+
+#family status
+mprobs <- sdatm %>%
+  group_by(agegroup) %>%
+  summarize(mar1 = mean(mar1), #get means by age group for each status
+            mar2 = mean(mar2),
+            mar3 = mean(mar3),
+            mar4 = mean(mar4)) %>%
+  dplyr::select(mar1, mar2, mar3, mar4) %>% #keep only perc's and convert to matrix
+  as.matrix()
+
+#education
+eduprobs <- sdatm %>%
+  group_by(agegroup) %>%
+  summarize(edu1d = mean(edu1d),
+            edu2d = mean(edu2d),
+            edu3d = mean(edu3d),
+            edu4d = mean(edu4d)) %>%
+  dplyr::select(edu1d, edu2d, edu3d, edu4d) %>%
+  as.matrix()
+
+#income mean and standard deviatiob
+yparams <- sdatm %>%
+  group_by(agegroup) %>%
+  summarize(ymean = mean(neYl),
+            ysd = sd(neYl)) %>%
+  dplyr::select(ymean, ysd) %>%
+  as.matrix()
+
+#DNat (currently not correlated with age in simulation function)
+#pdnat <- sdatm %>%
+#  group_by(agegroup) %>%
+#  summarize(ymean = mean(DNat)) %>%
+#  dplyr::select(ymean) %>%
+#  as.matrix()
+
+
+#get overall percentage of East-West (OW) and Dnat
+pdnat <- sum(sdatm$DNat==1)/nrow(sdatm)
+peawe <- sum(sdatm$OW==1)/nrow(sdatm)
+
+##find out coefficients for normal distribution
+gammaf <- gamlss(mPCS ~ age + agesq + neYl + DNat +
+                   edu2d + edu3d + edu4d + 
+                   mar2 + mar3 + mar4 + OW +
+                   re(random = ~1|bula), 
+                 sigma.formula = mPCS ~ age + agesq + neYl + DNat +
+                   edu2d + edu3d + edu4d + 
+                   mar2 + mar3 + mar4 + OW +
+                   re(random = ~1|bula), 
+                 data = soepf,
+                 weights = w8,
+                 family = NO(mu.link = "identity",
+                             sigma.link = "log"))
+f_mu_coef <- data.frame(t(as.matrix(gammaf$mu.coefficients[1:12])))
+f_sigma_coef <- data.frame(t(as.matrix(gammaf$sigma.coefficients[1:12])))
+
+gammam <- gamlss(mPCS ~ age + agesq + neYl + DNat +
+                   edu2d + edu3d + edu4d + 
+                   mar2 + mar3 + mar4 + OW +
+                   re(random = ~1|bula), 
+                 sigma.formula = mPCS ~ age + agesq + neYl + DNat +
+                   edu2d + edu3d + edu4d + 
+                   mar2 + mar3 + mar4 + OW +
+                   re(random = ~1|bula), 
+                 data = soepm,
+                 weights = w8,
+                 family = NO(mu.link = "identity",
+                             sigma.link = "log"))
+
+m_mu_coef <- data.frame(t(as.matrix(gammam$mu.coefficients[1:12])))
+m_sigma_coef <- data.frame(t(as.matrix(gammam$sigma.coefficients[1:12])))
+
+
+
+########Simulate Data#######
+simdat_soepm <- simdata_soep_fct(distr = "normal", N = 10000, mu_coef = m_mu_coef, sigma_coef = m_sigma_coef, 
+                             cutage = cutage, pmars = mprobs, pedus = eduprobs, yparams = yparams, 
+                             peawe = peawe, pdnat = pdnat, norm = TRUE, noise = TRUE)
+
+#write.csv(simdat_soepm, paste0(path, "/data/", "fulldatasim_norm", ".csv"), row.names = FALSE)
---
+++ simdata/soep_simdata_fct.R
@@ -0,0 +1,116 @@
+#note: if gamma distr. is wanted, the distr. parameters are still called
+#mu_coef and sigma_coef in the function.
+#mu_coef is then the shape parameter of the gamma distr
+#sigma_coef is then the scale parameter of the gamma distr
+
+simdata_soep_fct <- function(distr = c("normal", "gamma"), mu_coef, sigma_coef, N = 10000, cutage = c(0, seq(20, 100, by = 10)),
+                         pmars, pedus, yparams, peawe, pdnat, noise = TRUE, norm = TRUE){
+  
+  
+  #generate dataframe that will contain the simulated data
+  #add intercept column
+  simdat <- data.frame(X.Intercept. = rep(1, times = N))
+  
+  
+  #####simulate age variables#######
+  #first, age itself
+  #(distribution roughly based on true age distr in SOEP data)
+  age <- round(c(runif(N/4, 18, 40), runif(N/1.6 , 40, 78), runif(N/8, 78, 95)))
+  simdat$age <- age
+  #make agegroup variable 
+  #(used later to later on incorporate correlation btw age and other variables)
+  simdat$agegr <- as.numeric(cut(simdat$age, breaks = cutage, label = 1:(length(cutage)-1)))
+  #age squared
+  simdat$agesq <- (simdat$age)^2
+  
+  
+  
+  ####simulate categorical variables####
+  simdat$mar1 <- 0
+  simdat$mar2 <- 0
+  simdat$mar3 <- 0
+  simdat$mar4 <- 0
+  
+  #draw family status from a multinomial distribution for each age group
+  for (i in 1:(length(cutage)-1)){
+    pr <- pmars[i,] #get probabilities for the different family statuses from pmars matrix
+    num_age <- sum(simdat$agegr == i) #how many in that age group
+    mars <- t(rmultinom(n = num_age, size = 1, prob = pr))
+    simdat[simdat$agegr == i, 5:8] <- mars  #5:8 are the columns in simdat df for the married variables(sorry for ugly hardcoding)
+  }
+  
+  
+  #same exact procedure for education level
+  simdat$edu1d <- 0
+  simdat$edu2d <- 0
+  simdat$edu3d <- 0
+  simdat$edu4d <- 0
+  
+  for (i in 1:(length(cutage)-1)){
+    pr <- pedus[i,]
+    num_age <- sum(simdat$agegr == i)
+    edus <- t(rmultinom(n = num_age, size = 1, prob = pr))
+    simdat[simdat$agegr == i, 9:12] <- edus #9:12 are columns in df for the edu variables
+  }
+  
+  
+  #####simulate income variable#####
+  #again, by agegroup (from normal distribution)
+  simdat$neYl <- 0
+  for (i in 1:(length(cutage)-1)){
+    params <- yparams[i,]
+    num_age <- sum(simdat$agegr == i)
+    ys <- rnorm(num_age, mean = params[1], sd = params[2])
+    simdat[simdat$agegr == i, "neYl"] <- ys
+  }
+  
+  #add additional noise
+  if(noise){
+    e_t <- rnorm(N)
+    simdat$neYl <- simdat$neYl + e_t
+  }
+  
+  
+  ##simulate OW and DNat variables (without regard to age correlation, for now)
+  simdat$OW <- 0
+  OWdummy <- rbinom(N, 1, peawe)
+  simdat$OW[OWdummy == 1] <- 1
+  
+  simdat$DNat <- 0
+  DNatdummy <- rbinom(N, 1, pdnat)
+  simdat$DNat[DNatdummy == 1] <- 1
+  
+  
+  ###make dataframe into matrix to simulate health score
+  simdatmat <- as.matrix(dplyr::select(simdat, names(mu_coef))) #only keep relevant variables
+  #generate health score simulation values
+  
+  
+  if (distr == "normal"){
+    mu_vec <- simdatmat %*% t(as.matrix(mu_coef))
+    sigma_vec <- exp(simdatmat %*% t(as.matrix(sigma_coef)))
+    #simulate health score from normal distribution
+    simdat$mPCS <- rnorm(N, mu_vec, sigma_vec)
+    
+  } else if (distr == "gamma") {
+    #for the gamma distribution
+    scale_vec <- exp(simdatmat %*% t(as.matrix(mu_coef))) 
+    shape_vec <- exp(simdatmat %*% t(as.matrix(sigma_coef)))
+    #simulate health score from gamma distribution
+    simdat$mPCS <- rgamma(N, scale_vec, shape_vec)
+  }
+
+  simdat <- dplyr::select(simdat, c(names(mu_coef), "mPCS"))
+  simdat <- simdat[,-1] #remove intercept column 
+  
+  #normalize numerical variables
+  if(norm){
+    for (i in 1:3){
+      meani <- mean(simdat[,i])
+      stdi <- sd(simdat[,i])
+      simdat[,i] <- (simdat[,i] - meani)/stdi
+    }
+  }
+  
+  return(simdat)
+}
